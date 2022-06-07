1523acbcccb69d99f3f3eddf89148c24289b3b30
Dawid Olszewski <dawid.olszewski.dev@gmail.com>
2022-06-07 22:38:37 +0200

    menu visability toggle by means of butto in header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const menuBtn = document.querySelector('.menu')
-const menuCheckBox = menuBtn.querySelector('input')
+const menuBtn = document.querySelector('[data-menu-btn-label]')
+const menuCheckBox = document.querySelector('[data-menu-btn]')
+const menu = document.querySelector('[data-menu]')
 
 menuBtn.addEventListener('click', (e) => {
     menuBtn.classList.toggle('open-menu', menuCheckBox.checked)
+    menu.classList.toggle('open-menu', menuCheckBox.checked)
     // console.log(e.target, e.currentTarget)
 })
