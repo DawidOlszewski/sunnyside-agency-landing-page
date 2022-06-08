0b9943f9677bc2f352c6ee451feb38ddae23da80
Dawid Olszewski <dawid.olszewski.dev@gmail.com>
2022-06-08 16:41:53 +0200

    adding deleted js :)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ const menu = document.querySelector('[data-menu]')
 
 menuBtn.addEventListener('click', (e) => {
     menuBtn.classList.toggle('open-menu', menuCheckBox.checked)
-    menu.classList.toggle('open-menu', menuCheckBox.checked)
-    // console.log(e.target, e.currentTarget)
+
+    if (menuCheckBox.checked) {
+        menu.classList.toggle('invisible', false)
+        menu.classList.toggle('out', false)
+    } else {
+        menu.classList.toggle('out', true)
+        menu.addEventListener(
+            'animationend',
+            () => {
+                menu.classList.toggle('invisible', !menuCheckBox.checked)
+            },
+            { once: true }
+        )
+    }
 })
